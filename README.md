4101ee296af5bf4d8898b457929e76a4b85eff4d
Anuarder <anuar.ibraev97@gmail.com>
2018-06-12 02:20:22 +0600

    Fixed Bugs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # E-TWOW.KZ
 Landing page для Компании E-TWOW.KZ
+
--- vendor/sendMail/mail.php
+++ vendor/sendMail/mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$to = "@etwow.com.kz";
+$to = "example";
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 
-$subject = "Заявка на звонок с сайта E-TWOW.KZ";
-$message = "Имя: $name \nТелефон: $phone";
-
-mail($to, $subject, $message);
+$subject = "Заявка на звонок с сайта E-TWOW.KZ от $name";
+$message = "Имя: $name <br> Телефон: <a href=\"tel:$phone\">$phone</a>";
+$header = "Content-Type: text/html; charset=utf-8"; 
+mail($to, $subject, $message, $header);
 
 ?>
